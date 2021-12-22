13af3cc8f3f51a7ee29ed4e03f97af97d149f4e4
Collin D Didier <67519279+CDidier80@users.noreply.github.com>
2021-12-21 18:45:15 -0600

    Update example.ts

--- example.ts
+++ example.ts
@@ -1,17 +1,38 @@
 const phoneChannel = await ConversationChannel.where({
   name: "Phone"
 })
+  // Merges are for filtering and sorting nested queries. Do not add include() or select() methods.
   .merge({
     conversations: Conversation.where({ customerAppId: props.up.id })
   })
+  // Included models are always pluralized
+  // Models can be nested in a JSON-like hierarchy as far as a chain of relationships extends
+  // Provide an array to include several related models
   .includes([
-    {
-      conversations: "customerAppConversationAssignments"
+    // Use a string for a related model without any further nesting
+    "user",
+    // Use an object for including nested relationships. Each key and value represents another nested
+    // association to include in the query. 
+    // This query would return Conversations including their Assignments, each of which include their 
+    // Assignees, each of which includes their Credentials.
+    // Model traversal is: Conversation => Assignments => Assignees => Credentials
+    { 
+      conversations: {
+        assignments: {
+          assignees: "credentials"
+        }
+      }
     }
   ])
+  // Select the attributes you want returned from each model. This is the "Graph" part of Graphiti.
+  // All included models, regardless of their nested depth (above), are placed as top-level keys in
+  // the object passed to the select() method. 
+  // When you use select(), all attributes not explicitly selected are excluded from the query result.
   .select({
+    // The root-level Model goes here as well - not just the associations
     conversationChannels: "conversations",
     conversations: "recipient",
+    // Use an array to select multiple attributes.
     customerAppConversationAssignments: ["label", "isPrimary"]
   })
   .per(1)
